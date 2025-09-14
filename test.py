850fb7a8324a0d5558d88f663c3be4121d35c3b8
Demianiuk-Mykola <120207859+Demianiuk-Mykola@users.noreply.github.com>
2025-09-14 02:43:36 +0000

    first graph try

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
 import streamlit as st
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 st.write("Hello there here world1")
 
 #-------------------------------------------
-
+#loading everything to dataframe
 df = pd.read_csv('data/movie_ratings.csv')
-#st.write(df.head())
-df_cleared = df.dropna()
-#st.write(df_cleared)
-gb = df.groupby('genres')
+#getting rid of lines with NaN in 'genres'
+cleared = df.dropna()
+#creating new dataframe to store sanitized rows
+df_cleared = df[cleared].copy()
+#creating a mask with grouped_by results
+gb = df_cleared.groupby('genres')
+#dispalaying grouped_by results on streamlit page 
 st.write(gb['movie_id'].count())
+
+
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(
+        x=gb['movie_id'].count(),
+        y=gb
+    )
+)
+
+st.plotly_chart(fig, config = {'scrollZoom': False})
